49f18d36f55d6abc8dd6dfaff226f6aafd924a94
el3m3nTiC <90087363+el3m3nTik@users.noreply.github.com>
2021-12-16 19:20:50 +0200

    add labs

---
+++ lab 1.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <climits>
+
+int main()
+{
+    int Q1, P1, Q2, P2, A;
+    std::cin >> Q1 >> P1 >> Q2 >> P2 >> A;
+    int num = A / Q1 + (A % Q1 != 0);
+    int maxx = INT_MAX;
+    for (int i = 0; i < num; i++)
+    {
+        int a = A - i * Q1;
+        auto m = 0;
+        if (a <= 0) {
+            m = 0 + i * P1;
+        }
+        else {
+            m = (a / Q2 + (a % Q2 != 0)) * P2 + i * P1;
+        }
+        if (m < maxx) {
+            maxx = m;
+        }
+    }
+    std::cout << maxx << std::endl;
+}
