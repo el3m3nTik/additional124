16b1c92b1b43e9313b309f17098dcb03ebc7204d
Andrey Sitnikov <90087363+el3m3nTik@users.noreply.github.com>
2022-01-29 10:37:23 +0200

    Update lab 1.cpp

--- lab 1.cpp
+++ lab 1.cpp
@@ -8,8 +8,8 @@ int main()
 	int Amin = INT_MAX;
 	std::cin >> Q1 >> P1 >> Q2 >> P2 >> A;
 
-	for (int i = 0; i <= ceil(A / Q1); i++) {
-		for (int j = 0; j <= ceil(A / Q2); j++) {
+	for (int i = 0; i <= ceil(float(A) / float(Q1)); i++) {
+		for (int j = 0; j <= ceil(float(A) / float(Q2)); j++) {
 			if ((i * Q1 + j * Q2 >= A) && (i * P1 + j * P2 < Amin) && (i * P1 + j * P2 > 0))
 				Amin = i * P1 + j * P2;
 		}
